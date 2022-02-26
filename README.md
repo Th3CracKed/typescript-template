523cb7ef6f7477b786668996ce1aafcded2232bb
Driowya Abdelghafour <abdodriowya@gmail.com>
2022-02-26 16:50:16 +0100

    add style checking configs

---
+++ README.md
@@ -0,0 +1,7 @@
+# Typescript Node.js Template
+
+- Fast watch mode with `tsc-watch`
+- Fast jest config with `@swc/jest`
+- Eslint
+- Prettier
+- Editorconfig
--- src/lib/add.spec.ts
+++ src/lib/add.spec.ts
@@ -1,7 +1,7 @@
 import { add } from "./add";
 
 describe('test add', () => {
-    it('should return correct sum', () => {
-        expect(add(1, 2)).toEqual(3);
-    });
+  it('should return correct sum', () => {
+    expect(add(1, 2)).toEqual(3);
+  });
 });
